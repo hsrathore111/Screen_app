fd10047abfcee67f2d404654e9e4bdb7fc3d735b
himmat singh rathore <hsrathore111.hsr@gmail.com>
2018-10-25 17:28:30 +0530

    Initial commit

---
+++ app/src/main/java/com/example/himmat/screen_app/Splashscreen.java
@@ -0,0 +1,27 @@
+package com.example.himmat.screen_app;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Splashscreen extends AppCompatActivity {
+
+    Handler handler;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splashfile);
+
+        handler=new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(Splashscreen.this,MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },3000);
+
+    }
+}
